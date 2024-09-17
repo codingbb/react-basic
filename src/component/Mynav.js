276846aec398abe38ecd67f1b8dd1691b6398c5f
SCHcoding <codingb007@gmail.com>
2024-09-18 00:31:02 +0900

    shouldComponentUpdate 사용

--- src/component/Mynav.js
+++ src/component/Mynav.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class Mynav extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    console.log(
+      "shouldComponentUpdate 작동",
+      newProps.data, // 변경된 값
+      this.props.data // 변경전 값
+    );
+    if (this.props.data === newProps.data) {
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
     let lists = [];
     // console.log(this.props.data);
