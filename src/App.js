70e74fa9312bcb2d78167a45817930c952764b11
SCHcoding <codingb007@gmail.com>
2024-09-18 02:47:34 +0900

    리팩토링 및 update 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Myheader from "./component/Myheader";
 import Myarticle from "./component/Myarticle";
 import Mynav from "./component/Mynav";
 import CreateArticle from "./component/CreateArticle";
+import UpdateArticle from "./component/UpdateArticle";
 
 class App extends Component {
   constructor(props) {
@@ -39,11 +40,20 @@ class App extends Component {
       ],
     };
   }
-  render() {
+
+  getReadArticle() {
+    let idx = this.state.menus.findIndex(
+      (item) => item.id === this.state.selected_id
+    );
+    let data = this.state.menus[idx];
+    return data;
+  }
+
+  // 모드에 따라서 article을 바꾸는 걸 이 함수를 통해서 할 수 있다!
+  getArticles() {
     let _title,
       _desc,
       _article = null;
-
     // welcome 모드인 경우
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
@@ -59,18 +69,12 @@ class App extends Component {
       // read 모드인 경우
     } else if (this.state.mode === "read") {
       // menus 안에 있는 걸 item으로 받음
-      let idx = this.state.menus.findIndex(
-        (item) => item.id === this.state.selected_id
-      );
-      let data = this.state.menus[idx];
-      console.log(data);
-      _title = data.title;
-      _desc = data.desc;
+      let _data = this.getReadArticle();
       // console.log(idx);
       _article = (
         <Myarticle
-          title={_title}
-          desc={_desc}
+          title={_data.title}
+          desc={_data.desc}
           onChangeMode={(_mode) => {
             this.setState({
               mode: _mode,
@@ -94,12 +98,39 @@ class App extends Component {
             });
             this.setState({
               menus: _menus,
+              mode: "read",
+              selected_id: this.max_menu_id,
             });
           }}
         ></CreateArticle>
       );
-    }
+    } else if (this.state.mode === "update") {
+      let _content = this.getReadArticle();
 
+      _article = (
+        <UpdateArticle
+          data={_content}
+          onSubmit={(_title, _desc, _id) => {
+            // console.log(_title, _desc, _id);
+
+            let _menus = Array.from(this.state.menus);
+            _menus.forEach((item, index) => {
+              if (item.id === _id) {
+                _menus[index] = { id: _id, title: _title, desc: _desc };
+              }
+            });
+            this.setState({
+              menus: _menus,
+              mode: "read",
+            });
+          }}
+        ></UpdateArticle>
+      );
+    }
+    return _article;
+  }
+
+  render() {
     return (
       <div className="App">
         {/* <Myheader /> 이렇게 사용도 가능 */}
@@ -118,17 +149,10 @@ class App extends Component {
             this.setState({ mode: "read", selected_id: id });
           }}
         ></Mynav>
-        {/* <Myarticle
-          title={_title}
-          desc={_desc}
-          mode={this.state.mode}
-          onChangeMode={(_mode) => {
-            this.setState({
-              mode: _mode,
-            });
-          }}
-        ></Myarticle> */}
-        {_article}
+
+        {/* getArticles 함수가 실행된 결과물을 이 자리에 _article을 출력함 */}
+        {this.getArticles()}
+        {/* {_article} */}
         <hr />
         <div className="menu">
           <button
