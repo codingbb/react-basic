15a80e2b88e6515392292890a7d90b1d7a375951
SCHcoding <codingb007@gmail.com>
2024-09-19 14:51:45 +0900

    App, Mynav 함수형으로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,75 +1,65 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Myheader from "./component/Myheader";
 import Myarticle from "./component/Myarticle";
 import Mynav from "./component/Mynav";
 import CreateArticle from "./component/CreateArticle";
 import UpdateArticle from "./component/UpdateArticle";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.max_menu_id = 3;
-    this.state = {
-      mode: "welcome",
-      selected_id: 2,
-      welcome: {
-        title: "Welcome",
-        desc: "Welcome to FrontEnd",
-      },
-      subject: {
-        title: "프론트엔드 개발자 역량",
-        desc: "기본언어인 html, css, javascript부터 학습합니다.",
-      },
-      menus: [
-        {
-          id: 1,
-          title: "UI/UX 개발",
-          desc: "사용자 인터페이스와 사용자가 웹사이트르 이용하면 느끼고 생각하는 총체적 경험을 개발",
-        },
-        {
-          id: 2,
-          title: "재사용이 가능한 UI 개발",
-          desc: "앵귤러, 리액트, 뷰 등의 자바스크립트 프레임워크를 가지고 재사용할 수 있는 UI를 만든다.",
-        },
-        {
-          id: 3,
-          title: "애니메이션 구현",
-          desc: "CSS 또는 javascript를 사용해 다양한 효과의 애니메이션 구현한다.",
-        },
-      ],
-    };
-  }
+// function App() {} 으로 해도 됨
+const App = () => {
+  let max_menu_id = 3;
+  let welcome = {
+    title: "Welcome",
+    desc: "Welcome to FrontEnd",
+  };
+  let subject = {
+    title: "프론트엔드 개발자 역량",
+    desc: "기본언어인 html, css, javascript부터 학습합니다.",
+  };
+  const [mode, setMode] = useState("welcome");
+  const [selected_id, setSelectedId] = useState(2);
+  const [menus, setMenus] = useState([
+    {
+      id: 1,
+      title: "UI/UX 개발",
+      desc: "사용자 인터페이스와 사용자가 웹사이트르 이용하면 느끼고 생각하는 총체적 경험을 개발",
+    },
+    {
+      id: 2,
+      title: "재사용이 가능한 UI 개발",
+      desc: "앵귤러, 리액트, 뷰 등의 자바스크립트 프레임워크를 가지고 재사용할 수 있는 UI를 만든다.",
+    },
+    {
+      id: 3,
+      title: "애니메이션 구현",
+      desc: "CSS 또는 javascript를 사용해 다양한 효과의 애니메이션 구현한다.",
+    },
+  ]);
 
-  getReadArticle() {
-    let idx = this.state.menus.findIndex(
-      (item) => item.id === this.state.selected_id
-    );
-    let data = this.state.menus[idx];
+  // 함수 정리
+  const getReadArticle = () => {
+    let idx = menus.findIndex((item) => item.id === selected_id);
+    let data = menus[idx];
     return data;
-  }
+  };
 
-  // 모드에 따라서 article을 바꾸는 걸 이 함수를 통해서 할 수 있다!
-  getArticles() {
+  const getArticles = () => {
     let _title,
       _desc,
       _article = null;
     // welcome 모드인 경우
-    if (this.state.mode === "welcome") {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
+    if (mode === "welcome") {
+      _title = welcome.title;
+      _desc = welcome.desc;
       _article = (
-        <Myarticle
-          title={_title}
-          desc={_desc}
-          mode={this.state.mode}
-        ></Myarticle>
+        <Myarticle title={_title} desc={_desc} mode={mode}></Myarticle>
       );
 
       // read 모드인 경우
-    } else if (this.state.mode === "read") {
+    } else if (mode === "read") {
       // menus 안에 있는 걸 item으로 받음
-      let _data = this.getReadArticle();
+      let _data = getReadArticle();
       // console.log(idx);
       _article = (
         <Myarticle
@@ -79,52 +69,44 @@ class App extends Component {
             if (_mode === "delete") {
               if (window.confirm("정말 삭제할까요?")) {
                 // 배열을 새로 만들어서 _menus에 담는다 (원본을 건드리지 않기 위해서)
-                let _menus = Array.from(this.state.menus);
+                let _menus = Array.from(menus);
                 // 복사본의 하나하나의 아이템을 item으로 받아서 id랑 일치하는 것을 찾는다
-                let idx = _menus.findIndex(
-                  (item) => item.id === this.state.selected_id
-                );
+                let idx = _menus.findIndex((item) => item.id === selected_id);
                 console.log("idx ", idx);
                 // 이제 복사본에서 찾은 idx를 삭제한다
                 _menus.splice(idx, 1);
                 // 원본에 복사본을 넣어준다
-                this.setState({
-                  mode: "welcome",
-                  menus: _menus,
-                });
+                setMode("welcome");
+                setMenus(_menus);
               }
             } else {
-              this.setState({
-                mode: _mode,
-              });
+              setMode(_mode);
             }
           }}
         ></Myarticle>
       );
 
       // crate 모드인 경우
-    } else if (this.state.mode === "create") {
+    } else if (mode === "create") {
       _article = (
         <CreateArticle
           onSubmit={(_title, _desc) => {
             // console.log(_title, _desc);
-            this.max_menu_id += 1;
+            max_menu_id += 1;
 
-            let _menus = this.state.menus.concat({
-              id: this.max_menu_id,
+            let _menus = menus.concat({
+              id: max_menu_id,
               title: _title,
               desc: _desc,
             });
-            this.setState({
-              menus: _menus,
-              mode: "read",
-              selected_id: this.max_menu_id,
-            });
+            setMenus(_menus);
+            setMode("read");
+            setSelectedId(max_menu_id);
           }}
         ></CreateArticle>
       );
-    } else if (this.state.mode === "update") {
-      let _content = this.getReadArticle();
+    } else if (mode === "update") {
+      let _content = getReadArticle();
 
       _article = (
         <UpdateArticle
@@ -132,63 +114,57 @@ class App extends Component {
           onSubmit={(_title, _desc, _id) => {
             // console.log(_title, _desc, _id);
 
-            let _menus = Array.from(this.state.menus);
+            let _menus = Array.from(menus);
             _menus.forEach((item, index) => {
               if (item.id === _id) {
                 _menus[index] = { id: _id, title: _title, desc: _desc };
               }
             });
-            this.setState({
-              menus: _menus,
-              mode: "read",
-            });
+            setMenus(_menus);
+            setMode("read");
           }}
         ></UpdateArticle>
       );
     }
     return _article;
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        {/* <Myheader /> 이렇게 사용도 가능 */}
-        <Myheader
-          title={this.state.subject.title}
-          desc={this.state.subject.desc}
-          // 함수를 자식에게 넘김
-          onChangeMode={() => {
-            this.setState({ mode: "welcome" });
-          }}
-        ></Myheader>
-        <Mynav
-          data={this.state.menus}
-          onChangePage={(id) => {
-            // debugger;
-            this.setState({ mode: "read", selected_id: id });
-          }}
-        ></Mynav>
+  return (
+    <div className="App">
+      <Myheader
+        title={subject.title}
+        desc={subject.desc}
+        // 함수를 자식에게 넘김
+        onChangeMode={() => {
+          setMode("welcome");
+        }}
+      ></Myheader>
 
-        {/* getArticles 함수가 실행된 결과물을 이 자리에 _article을 출력함 */}
-        {this.getArticles()}
-        {/* {_article} */}
-        <hr />
-        <div className="menu">
-          <button
-            type="button"
-            className="primary"
-            onClick={() => {
-              this.setState({
-                mode: "create",
-              });
-            }}
-          >
-            Create task
-          </button>
-        </div>
+      <Mynav
+        data={menus}
+        onChangePage={(id) => {
+          setMode("read");
+          setSelectedId(id);
+          // debugger;
+        }}
+      ></Mynav>
+
+      {/* getArticles 함수가 실행된 결과물을 이 자리에 _article을 출력함 */}
+      {getArticles()}
+      <hr />
+      <div className="menu">
+        <button
+          type="button"
+          className="primary"
+          onClick={() => {
+            setMode("create");
+          }}
+        >
+          Create task
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/component/Mynav.js
+++ src/component/Mynav.js
@@ -1,24 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
-class Mynav extends Component {
-  shouldComponentUpdate(newProps, newState) {
-    // console.log(
-    //   "shouldComponentUpdate 작동",
-    //   newProps.data, // 변경된 값
-    //   this.props.data // 변경전 값
-    // );
-    if (this.props.data === newProps.data) {
-      return false;
-    }
-
-    return true;
-  }
-
-  render() {
-    let lists = [];
-    // console.log(this.props.data);
-    const data = this.props.data;
-
+const Mynav = ({ data, onChangePage }) => {
+  // 초기값은 부모로부터 받은 빈배열
+  const [list, setList] = useState([]);
+  let lists = [];
+  const getList = () => {
     data.forEach((item) => {
       lists.push(
         <li key={item.id}>
@@ -26,7 +12,7 @@ class Mynav extends Component {
             href="/"
             onClick={(e) => {
               e.preventDefault();
-              this.props.onChangePage(item.id);
+              onChangePage(item.id);
             }}
           >
             {item.title}
@@ -34,13 +20,60 @@ class Mynav extends Component {
         </li>
       );
     });
+    setList(lists);
+  };
 
-    return (
-      <nav>
-        <ul>{lists}</ul> {/* 동적으로 생성된 리스트를 렌더링 */}
-      </nav>
-    );
-  }
-}
+  useEffect(() => {
+    getList(); // 가독성을 위해 함수를 호출해서 ㄱㄱ
+  }, [data]); // 부모로부터 받은 data가 변경될 때만 실행
+  return (
+    <nav>
+      <ul>{list}</ul>
+    </nav>
+  );
+};
 
 export default Mynav;
+
+// class Mynav extends Component {
+//   shouldComponentUpdate(newProps, newState) {
+//     // console.log(
+//     //   "shouldComponentUpdate 작동",
+//     //   newProps.data, // 변경된 값
+//     //   this.props.data // 변경전 값
+//     // );
+//     if (this.props.data === newProps.data) {
+//       return false;
+//     }
+
+//     return true;
+//   }
+
+//   render() {
+//     let lists = [];
+//     // console.log(this.props.data);
+//     const data = this.props.data;
+
+//     data.forEach((item) => {
+//       lists.push(
+//         <li key={item.id}>
+//           <a
+//             href="/"
+//             onClick={(e) => {
+//               e.preventDefault();
+//               this.props.onChangePage(item.id);
+//             }}
+//           >
+//             {item.title}
+//           </a>
+//         </li>
+//       );
+//     });
+
+//     return (
+//       <nav>
+//         <ul>{lists}</ul> {/* 동적으로 생성된 리스트를 렌더링 */}
+//       </nav>
+//     );
+//   }
+// }
